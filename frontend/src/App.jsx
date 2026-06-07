981502579c47e794df2a410412a08556b6df1f84
Egot Gubaidulin <egorgubajdulin146@gmail.com>
2026-06-07 08:09:57 +0500

    подключения

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -977,7 +977,7 @@ function DashboardApp() {
       await getNewScreen(screenCode)
       setScreenCodeMessage('Новый экран найден, можно обновить список устройств.')
     } catch {
-      setScreenCodeMessage('Экран с этим кодом не найден.')
+      setScreenCodeMessage('Ваш пк стал телеком сори за такие переменны, это подарок от нас вам')
     }
   }
 
